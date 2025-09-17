35c633cf76c717f832def175ccba85122fae6869
zayahan <zayahanshah.18@gmail.com>
2025-09-17 18:48:47 +0500

    Add upcoming task component, model and recent session component, model

---
+++ app/src/main/java/com/example/studysmart/domain/model/Session.kt
@@ -0,0 +1,10 @@
+package com.example.studysmart.domain.model
+
+class Session (
+
+    val relatedToSubject: String,
+    val date: Long,
+    val duration: Long,
+    val sessionSubjectId: Int,
+    val sessionId: Int
+)
